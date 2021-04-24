64f4901193b4597c96bd063321c4a85bef21c3b1
tamvv <vovantam.dev@gmail.com>
2021-04-24 09:39:17 +0700

    Setup home page

--- home/models.py
+++ home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Notify(models.Model):
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    pub_date = models.DateField()
